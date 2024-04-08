d0db58192365da9acca6c5b77995c1c3bd36ca97
Luis <tony_articuno@hotmail.com>
2024-04-08 21:27:44 +0200

    Add project files.

---
+++ KeyboardApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace KeyboardApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ KeyboardApp/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace KeyboardApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnNewControl(object sender, EventArgs e)
+        {
+            var stack = new HorizontalStackLayout() { Spacing = 20 };
+            var label = new Label() { Text = "New item" };
+            var entry = new Entry() { Placeholder = "Add text here" };
+            var button = new Button() { Text = "Click me" };
+            stack.Children.Add(label);
+            stack.Children.Add(entry);
+            stack.Children.Add(button);
+
+            stackContainer.Children.Add(stack);
+        }
+
+        private void OnDeleteControl(object sender, EventArgs e)
+        {
+            if (stackContainer.Children.Count > 0) 
+                stackContainer.Children.RemoveAt(
+                    stackContainer.Children.Count - 1);
+        }
+    }
+
+}
